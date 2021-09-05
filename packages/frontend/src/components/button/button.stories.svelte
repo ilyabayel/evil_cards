f15179cfc2aba38aae2c9605d6eb07aeda8d4c90
Kozlov Ilya <ilya@kozlov.dev>
2021-09-05 20:21:04 +0600

    feat(frontend): add button

---
+++ packages/frontend/src/components/button/button.stories.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import Button from "./button.svelte";
+</script>
+
+<Meta
+    title="Button"
+    component={Button}
+    argTypes={{
+        label: { control: "text" },
+        onClick: { action: "onClick" },
+        type: {
+            type: "select",
+            options: ["primary", "secondary"],
+            defaultValue: "primary",
+        },
+    }}
+/>
+
+<Template let:args>
+    <Button {...args} />
+</Template>
+
+<Story args={{ label: "label" }} name="Default" />
