5557c914c4511b6bcffaa335e37349da2ca41913
Kozlov Ilya <ilya@kozlov.dev>
2021-09-05 20:34:03 +0600

    feat(frontend): add Input

---
+++ packages/frontend/src/components/input/input.stories.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import Input from "./input.svelte";
+</script>
+
+<Meta
+    title="Input"
+    component={Input}
+    argTypes={{
+        value: { control: "text" },
+        onChange: { action: "onChange" },
+        type: {
+            type: "select",
+            options: ["text", "number"],
+            defaultValue: "text",
+        },
+    }}
+/>
+
+<Template let:args>
+    <Input {...args} />
+</Template>
+
+<Story args={{ value: "Some value" }} name="Default" />
---
+++ packages/frontend/src/components/input/input.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let value: string;
+    export let onChange: (value: string) => void;
+    export let type: "text" | "number" = "text";
+</script>
+
+<input {type} {value} on:input={(e) => onChange(e.currentTarget.value)} />
+
+<style>
+    input {
+        background: transparent;
+        border: 2px solid rgba(255, 255, 255, 0.5);
+        padding: 1em;
+        border-radius: 6px;
+        font-size: 1.6em;
+        color: white;
+        text-shadow: 0 0 2px black;
+    }
+    input:focus {
+        outline: none;
+        background: rgba(255, 255, 255, 0.15);
+    }
+</style>
