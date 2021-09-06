7a46f44cb45d929bd319a46cc5ed768e33951e14
Kozlov Ilya <ilya@kozlov.dev>
2021-09-06 20:47:41 +0600

    feat(frontend): add OptionCard

---
+++ packages/frontend/src/components/option-card/option-card.stories.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import OptionCard from "./option-card.svelte";
+    import { Option } from "../../models/option";
+</script>
+
+<Meta
+    title="Option Card"
+    component={OptionCard}
+    argTypes={{
+        body: { type: Option },
+        onClick: { action: "onClick" },
+    }}
+/>
+
+<Template let:args>
+    <OptionCard {...args} />
+</Template>
+
+<Story
+    args={{
+        body: new Option("1", "some option"),
+    }}
+    name="Default"
+/>
