38ccace8820971d705b728bc40e7385bee329636
Kozlov Ilya <ilya@kozlov.dev>
2021-09-07 21:06:03 +0600

    refactor(frontend): minor changes

---
+++ packages/frontend/src/app.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Router, Route } from "svelte-navigator";
+  import Frame from "./components/frame/frame.svelte";
+  import HomeScreen from "./screens/home-screen.svelte";
+  import PlayScreen from "./screens/play-screen.svelte";
+  import "./global-styles.css";
+</script>
+
+<main>
+  <Frame>
+    <Router>
+      <Route path="/"><HomeScreen /></Route>
+      <Route path="/play/:roomId"><PlayScreen /></Route>
+    </Router>
+  </Frame>
+</main>
