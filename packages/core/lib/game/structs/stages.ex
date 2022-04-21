8fd1b3382f8f2ad9891bf1f38687cb4ee32de118
Ilya <i.kozlov.dev@gmail.com>
2022-04-21 22:12:59 +0600

    perf: small optimization

--- packages/core/lib/game/structs/stages.ex
+++ packages/core/lib/game/structs/stages.ex
@@ -1,13 +1,20 @@
 defmodule Game.Stages do
-  def wait, do: "wait"
-  def prepare, do: "prepare"
-  def play, do: "play"
-  def vote, do: "vote"
-  def result, do: "result"
 
-  def get_next_stage("wait"), do: "prepare"
-  def get_next_stage("prepare"), do: "play"
-  def get_next_stage("play"), do: "vote"
-  def get_next_stage("vote"), do: "result"
-  def get_next_stage("result"), do: "prepare"
+  @wait "wait"
+  @prepare "prepare"
+  @play "play"
+  @vote "vote"
+  @result "result"
+
+  def wait, do: @wait
+  def prepare, do: @prepare
+  def play, do: @play
+  def vote, do: @vote
+  def result, do: @result
+
+  def get_next_stage("wait"), do: @prepare
+  def get_next_stage("prepare"), do: @play
+  def get_next_stage("play"), do: @vote
+  def get_next_stage("vote"), do: @result
+  def get_next_stage("result"), do: @prepare
 end
