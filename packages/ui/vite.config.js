dca59c4a28189635debb987657aede84dcffcbac
Ilya <i.kozlov.dev@gmail.com>
2022-05-01 12:47:27 +0600

    refactor: delete svelte ui project

--- packages/ui/vite.config.js
+++ packages/ui/vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import createReScriptPlugin from '@jihchi/vite-plugin-rescript';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  resolve: {
-    alias: {
-      'src': './src'
-    }
-  }
-})
+  plugins: [reactRefresh(), createReScriptPlugin()],
+});
